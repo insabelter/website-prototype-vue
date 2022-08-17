6336241d9292d37fe44771bf9b7afdda3f90209a
Insa Belter <insa.belter@atos.net>
2022-08-17 16:20:29 +0200

    new grocery theme

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
 import { reactive } from 'vue';
 const state = reactive({
-  tags: ['Yellow', 'Red']
+  groceries: ['Mehl', 'Eier', 'Birnen', 'Mehl']
 })
+
+function sortGroceries() {
+  var sortedGroceries = [...new Set(state.groceries)].sort();
+  return sortedGroceries;
+}
+
 </script>
 
 <template>
-  <p>This is the Vue Test Website</p>
-  <Card title="Color Input">
-    <TagInput v-model:tags="state.tags" placeholder="Enter a color!"></TagInput>
+  <h1>Testwebseite</h1>
+  <h2>Prototyp der Technologie Vue.js</h2>
+
+  <Card title="Einkaufsliste">
+    <TagInput v-model:tags="state.groceries" placeholder="Zutat hinzufügen"></TagInput>
   </Card>
-  <p>Resulting Tags:</p>
-  <ul>
-    <li v-for="tag in state.tags">{{ tag }}</li>
-  </ul>
+
+  <Card title="Sortierte Zutaten:">
+    <ul>
+      <li v-for="tag in sortGroceries()">{{ tag }}</li>
+    </ul>
+  </Card>
 </template>
 
 <style>
